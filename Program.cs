356ac9a514213cb67926be93367c326047e8500e
Brian Brice <bbrice@gmail.com>
2017-01-19 12:40:39 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svn_clean
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] dirs = (args.Length > 0 ? args : new[] { "." });
+
+            foreach (var dir in dirs)
+            {
+                try
+                {
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An exception was thrown: " + e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+svn-clean
+================
+A utility for cleaning a SVN checkout by removing files that are not part of
+the repository.
+
+The current project page is located here:
+<https://github.com/bbrice/svn-clean>
+
+Building
+--------
+At the moment, Visual Studio 2013.5 is used for this project.
+
+Usage
+-----
+This application can take directory paths as arguments.  If no path is given,
+then the current directory is used.
+
+	gaia:~$ svn-clean svn-project
+
+License
+-------
+`svn-clean` is licensed under the BSD license. See
+[LICENSE.txt](LICENSE.txt) for more info.
